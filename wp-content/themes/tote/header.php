4ab3c38d321340050f80be3187f15c94f8b1486a
Tran IT <tranit1209@gmail.com>
2015-04-16 08:40:41 +0700

    ok

---
+++ wp-content/themes/tote/header.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * The Header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	
+	
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+	<meta property="og:image" content="http://www.tote-app.com/icon.png"/>
+
+	<meta name="twitter:image" content=”http://www.tote-app.com/icon.png”>
+	<meta name="twitter:card" content="app">
+	<meta name="twitter:site" content="@tote_app">
+	<meta name="twitter:app:country" content="US">
+	<meta name="twitter:app:name:iphone" content="tote-your-friend-with-a-truck">
+	<meta name="twitter:app:id:iphone" content="804968071">
+	<meta name="twitter:app:name:googleplay" content="com.hyprtree.umitruck">
+	<meta name="twitter:app:id:googleplay" content="com.hyprtree.umitruck">
+	<meta name=”twitter:title” content="Tote - Your Friend with a Truck | On Demand Delivery | Free App">
+	<meta name="twitter:description" content="Tote is your friend with a truck. We get your stuff delivered with the help of people and their trucks. Don't rent a truck, get a Tote! Serving Metro Atlanta area.">
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Tote is your friend with a truck. We get your stuff delivered with the help of people and their trucks. Don't rent a truck, get a Tote! Serving Atlanta.">
+
+	<link rel="icon" type="image/png" href="images/toti-logo.png" sizes="160x160">
+
+	<title>Tote - Your Friend with a Truck | On Demand Delivery | Serving Atlanta</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="css/jquery-ui.css">
+	<script src="js/jquery-1.10.2.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	
+	
+	
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<?php if ( get_header_image() ) : ?>
+	<div id="site-header">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+		</a>
+	</div>
+	<?php endif; ?>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-main">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+			<div class="search-toggle">
+				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
+			</div>
+
+			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
+				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+			</nav>
+		</div>
+
+		<div id="search-container" class="search-box-wrapper hide">
+			<div class="search-box">
+				<?php get_search_form(); ?>
+			</div>
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
